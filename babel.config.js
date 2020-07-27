ae393c6de9436ddd486da84aa7ad57342053a38d
Rafael Mello Campanari <lab.rafamello@gmail.com>
2020-07-27 09:01:55 -0300

    fix ci | require on .gitignored folder

--- babel.config.js
+++ babel.config.js
@@ -1,1 +1,24 @@
-module.exports = require('./lib/babel.config')
+module.exports = {
+    presets: [
+        [
+            '@babel/env',
+            {
+                useBuiltIns: false,
+                shippedProposals: true,
+                targets: {
+                    browsers: ['ie >= 11']
+                }
+            }
+        ],
+        '@babel/react',
+        '@babel/preset-typescript',
+        [
+            'const-enum',
+            {
+                transform: 'constObject'
+            }
+        ],
+        '@babel/flow'
+    ],
+    plugins: [require('babel-plugin-styled-components')]
+}
